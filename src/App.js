4f0e792026c0e08ad943b2e8936a9bb6b484dd79
Akhil Mishra <akhilmishra184@gmail.com>
2023-09-26 12:55:05 +0530

    copy card file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import InfiniteScrollView from "./Components/InfiniteScrollView";
 
 function App() {
   return (
     <div className="App">
-     Hello
+      <InfiniteScrollView />
     </div>
   );
 }
---
+++ src/Components/InfiniteScrollView.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import Card from "./Card";
+
+const API_URL = "app-api/v1/photo-gallery-feed-page/page";
+const InfiniteScrollView = ({ searchText }) => {
+  return (
+    <>
+      InfiniteScrollView
+      {/* <Card data={filteredData} loading={loading} /> */}
+    </>
+  );
+};
+export default InfiniteScrollView;
